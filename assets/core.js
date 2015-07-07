782375373e2fb998dd4dbad1556ab88607e847ef
Kit <kittsville@googlemail.com>
2015-07-07 20:10:25 +0100

    Added editing of key values/labels
    The value of keys can now be changed, so pressing them types the custom
    value rather than their default behaviour
    Added WordPress Dashicons
    Added license information for Dashicons

--- assets/core.js
+++ assets/core.js
@@ -2,33 +2,38 @@ var Chomskey = {
 	// Settings
 	s: {
 		keyboardWrap:	$('div#keyboard'),
-		keyElements:	{},
 		typingArea:		$('div#typing-area textarea'),
 		allowDefaulting:true,							// Whether to default to a key's normal behaviour if no mapping is found
 		keyMap:			{},
+		keyElements:	{},
 		gun:			'good',
 		penis:			'evil',
 	},
 	
 	init: function() {
 		this.s.keyboardWrap.find('a').each(function(i, key) {
-			Chomskey.s.keyElements[key.getAttribute('key')] = $(key);
+			var keyCode = Chomskey.getKeyCode(key);
+			
+			if (keyCode) {
+				Chomskey.s.keyElements[keyCode] = $(key);
+			}
 		});
 		
 		this.bindUIActions();
 	},
 	
 	bindUIActions: function() {
-		this.s.typingArea.keypress(function(event) {
-			Chomskey.typeKey(event);
-		});
-		
 		this.s.typingArea.keydown(function(event) {
 			Chomskey.highlightKey(event.which);
+			Chomskey.typeKey(event);
 		});
 		
 		this.s.typingArea.keyup(function(event) {
 			Chomskey.unhighlightKey(event.which);
+		});
+		
+		this.s.keyboardWrap.on('click', 'a', function(event) {
+			editKey.openWindow(event.currentTarget);
 		});
 	},
 	
@@ -84,8 +89,93 @@ var Chomskey = {
 			return false;
 		}
 	},
+	
+	getKeyCode: function(keyElement) {
+		if (keyElement instanceof $) {
+			return keyElement.prop('key');
+		} else {
+			return keyElement.getAttribute('key');
+		}
+	},
+	
+	updateKey: function(keyCode, value, label) {
+		Chomskey.s.keyMap[keyCode] = value;
+		
+		Chomskey.mapKeyElement(keyCode).text(label);
+	},
+};
+
+var editKey = {
+	// Settings
+	s: {
+		overlay:		$('div#edit-key-overlay'),
+		window:			$('div#edit-key-window'),
+		closeButton:	$('div#close-edit-key'),
+		cancelButton:	$('a#cancel-edit'),
+		updateButton:	$('a#update-key'),
+		valueInput:		$('input#key-value'),
+		labelInput:		$('input#key-label'),
+		keyNumDisplay:	$('h2#key-number'),
+		keyIDDisplay:	$('h4#key-id'),
+		keyCode:		0,
+	},
+	
+	init: function() {
+		this.bindUIActions();
+	},
+	
+	bindUIActions: function() {
+		this.s.updateButton.click(editKey.saveKey);
+		
+		this.s.closeButton.click(editKey.closeWindow);
+		this.s.overlay.click(editKey.closeWindow);
+		this.s.cancelButton.click(editKey.closeWindow);
+		this.s.window.click(function(e) {e.stopPropagation();}); // Stops clicking the window acting like clicking the dark overlay
+	},
+	
+	clearWindow: function() {
+		editKey.s.keyNumDisplay.text('');
+		editKey.s.keyIDDisplay.text('');
+		editKey.s.valueInput.val('');
+		editKey.s.labelInput.val('');
+	},
+	
+	closeWindow: function() {
+		editKey.s.overlay.fadeOut(100, editKey.clearWindow);
+	},
+	
+	openWindow: function(keyElement) {
+		var keyValue, keyCode = Chomskey.getKeyCode(keyElement);
+		
+		if (!keyCode) {
+			return;
+		}
+		
+		editKey.s.keyCode = keyCode;
+		
+		keyValue = Chomskey.mapKey(keyCode);
+		
+		if (typeof keyValue !== 'string') {
+			keyValue = '';
+		}
+		
+		editKey.s.keyNumDisplay.text(keyCode);
+		editKey.s.keyIDDisplay.text(keyElement.id);
+		editKey.s.labelInput.val(keyElement.text);
+		editKey.s.valueInput.val(keyValue);
+		
+		editKey.s.window.fadeIn(100);
+		editKey.s.overlay.fadeIn(100);
+	},
+	
+	saveKey: function() {
+		Chomskey.updateKey(editKey.s.keyCode, editKey.s.valueInput.val(), editKey.s.labelInput.val());
+		
+		editKey.closeWindow();
+	},
 };
 
 $(function() {
 	Chomskey.init();
+	editKey.init();
 });
