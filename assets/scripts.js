kittsville/Chomskey

commit a132eb60fbf2bbe146c44210787ded5dcc3967f2

Added ctrl key forcing default behaviour
Allows copying/pasting to textarea, closes #1

assets/scripts.js
@@ -91,6 +91,10 @@ var Chomskey = {
 	},
 	
 	typeKey: function(event) {
+		if (event.ctrlKey) {
+			return;
+		}
+		
 		var keyCharacter, keyCode = event.which;
 		
 		if (Chomskey.s.shift) {
@@ -105,8 +109,6 @@ var Chomskey = {
 			event.preventDefault();
 			
 			this.s.typingArea.val(this.s.typingArea.val() + keyCharacter);
-		} else {
-			return;
 		}
 	},
 	

commit 5ea0b88bdb8b892f32a7864217e1e5af91a625c8

Fixed Alt Gr mappings
Bug caused by a132eb60fbf2bbe146c44210787ded5dcc3967f2. Close #22

assets/scripts.js
@@ -122,7 +122,7 @@ var Keyboard = {
 	},
 	
 	typeKey: function(event) {
-		if (event.ctrlKey) {
+		if (event.ctrlKey && !event.altKey) {
 			return;
 		}
 		